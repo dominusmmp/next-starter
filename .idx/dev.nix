25dfabc9dd10b7a890a64811b42c573638f3f6a7
dominus <59709069+dominusmmp@users.noreply.github.com>
2024-07-02 14:12:39 -0700

    feat: add support for project idx and nix env

---
+++ .idx/dev.nix
@@ -0,0 +1,31 @@
+{ pkgs }: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.bun
+  ];
+  idx.extensions = [ ];
+  env = {
+    PORT = 3000;
+  };
+  idx.workspace.onCreate = {
+    deps-install = "bun install";
+  };
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "bun"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
