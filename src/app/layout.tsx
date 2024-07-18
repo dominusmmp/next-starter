0b6cc5cc44f5a496e399bcd7bafb3173eb5acdfc
dominus <59709069+dominusmmp@users.noreply.github.com>
2024-07-18 04:43:06 -0700

    chore: split up styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/lib/util/cn';
+import '@/style/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
