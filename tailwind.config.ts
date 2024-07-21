249722fb665b17f1ae64f85c360f6428ef18e8c5
dominus <59709069+dominusmmp@users.noreply.github.com>
2024-07-21 03:53:32 -0700

    feat: add base color palette based on tw colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
 import defaultTheme from 'tailwindcss/defaultTheme';
 import plugin from 'tailwindcss/plugin';
 
@@ -12,6 +13,70 @@ const config: Config = {
     extend: {
       fontFamily: {
         sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        light: {
+          background: {
+            DEFAULT: colors.white,
+            half: colors.neutral[100],
+            full: colors.neutral[200],
+          },
+          foreground: {
+            DEFAULT: colors.neutral[800],
+            half: colors.neutral[900],
+            full: colors.neutral[950],
+          },
+          title: colors.neutral[950],
+          body: colors.neutral[800],
+          subtitle: colors.neutral[700],
+          muted: colors.neutral[500],
+          active: colors.neutral[400],
+          inactive: colors.neutral[300],
+          disabled: colors.neutral[200],
+        },
+        dark: {
+          background: {
+            DEFAULT: colors.black,
+            half: colors.neutral[900],
+            full: colors.neutral[800],
+          },
+          foreground: {
+            DEFAULT: colors.neutral[200],
+            half: colors.neutral[100],
+            full: colors.neutral[50],
+          },
+          title: colors.neutral[50],
+          body: colors.neutral[200],
+          subtitle: colors.neutral[300],
+          muted: colors.neutral[500],
+          active: colors.neutral[600],
+          inactive: colors.neutral[700],
+          disabled: colors.neutral[800],
+        },
+        primary: {
+          DEFAULT: colors.blue[500],
+          ...colors.blue,
+        },
+        secondary: {
+          DEFAULT: colors.fuchsia[500],
+          ...colors.fuchsia,
+        },
+        success: {
+          DEFAULT: colors.emerald[500],
+          ...colors.emerald,
+        },
+        info: {
+          DEFAULT: colors.blue[500],
+          ...colors.blue,
+        },
+        warning: {
+          DEFAULT: colors.amber[500],
+          ...colors.amber,
+        },
+        error: {
+          DEFAULT: colors.rose[500],
+          ...colors.rose,
+        },
       },
     },
   },
