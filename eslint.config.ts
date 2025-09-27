4daea435e8656fc7939bebc376c0feaa299ccb2c
dominusmmp <59709069+dominusmmp@users.noreply.github.com>
2025-09-27 16:57:23 -0400

    chore: update dependencies and eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,7 +4,6 @@ import { fileURLToPath } from 'url';
 
 import js from '@eslint/js';
 import ts from 'typescript-eslint';
-// import tailwind from 'eslint-plugin-tailwindcss'; // better to use but has conflict with tailwindcss 4 as of now
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,40 +13,24 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
     ignores: [
-      'node_modules/*',
+      'node_modules/**',
+      '.next/*',
+      'out/*',
       'build/*',
       'coverage/*',
       'dist/*',
-      'out/*',
-      '.next/*',
       'public/*',
       'package-lock.json',
       'bun.lockb',
+      'next-env.d.ts',
       '*.md',
     ],
   },
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
-  js.configs.recommended,
-  ...ts.configs.recommended,
-  // ...tailwind.configs['flat/recommended'],
-  // {
-  //   settings: {
-  //     tailwindcss: {
-  //       // These are the default values but feel free to customize
-  //       callees: ['classnames', 'clsx', 'ctl', 'cn', 'cva'],
-  //       config: 'tailwind.config.ts',
-  //       cssFiles: ['**/*.css', '!**/node_modules', '!**/.*', '!**/dist', '!**/build'],
-  //       cssFilesRefreshRate: 5_000,
-  //       removeDuplicates: true,
-  //       skipClassAttribute: false,
-  //       whitelist: [],
-  //       tags: [], // can be set to e.g. ['tw'] for use in tw`bg-blue`
-  //       classRegex: '^class(Name)?$', // can be modified to support custom attributes. E.g. "^tw$" for `twin.macro`
-  //     },
-  //   },
-  // },
 ];
 
 export default eslintConfig;
